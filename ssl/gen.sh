dfa3203ad0dba3bb09f591218bc657a9904a80ae
Karim Alibhai <karim@alibhai.co>
2017-02-23 18:43:01 -0500

    Auto answer csr questions from config

--- ssl/gen.sh
+++ ssl/gen.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+# config
 MOD=8192
+CN=$( node -e 'console.log(require("../config.json").SERVER_HOST)' )
+
+##################################
+
+echo ""
+echo "GENERATING SERVER CERTIFICATE PAIR"
+echo ""
 
 # generate private key
 openssl genrsa -out server.key $MOD
 
 # generate signing request
-openssl req -new -sha256 -key server.key -out server.csr
+openssl req -new -sha256 -key server.key -out server.csr -subj "/CN=$CN"
 
 # remove passphrase from key
 mv server.key server.key.org
@@ -17,11 +25,15 @@ openssl x509 -req -in server.csr -signkey server.key -out server.crt
 
 ##################################
 
+echo ""
+echo "GENERATING CLIENT CERTIFICATE PAIR"
+echo ""
+
 # generate private key
 openssl genrsa -out client.key $MOD
 
 # generate signing request
-openssl req -new -sha256 -key client.key -out client.csr
+openssl req -new -sha256 -key client.key -out client.csr -subj "/CN=$CN"
 
 # remove passphrase from key
 mv client.key client.key.org
